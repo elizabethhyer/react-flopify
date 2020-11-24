1e91e528b16133e3fd417e100b57afaf4d29d467
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-11-24 17:14:06 -0500

    refactors movies_controller in flopify-api

--- flopify-api/app/controllers/api/v1/movies_controller.rb
+++ flopify-api/app/controllers/api/v1/movies_controller.rb
@@ -1,11 +1,15 @@
 class Api::V1::MoviesController < ApplicationController
+
+    before_action :set_genre
+
     def index
-        movies = Movie.all
-        render json: movies 
+        #render all movies
+        #render movies associated with a genre
+        render json: @genre.movies 
     end 
 
     def create
-        movie = Movie.new(movie_params)
+        movie = @genre.movie.build(movie_params)
         if movie.save
             render json: movie
         else 
@@ -14,6 +18,7 @@ class Api::V1::MoviesController < ApplicationController
     end
 
     def show
+        # movie = @genre.movies.find(by(id: params[:id]))
         movie = Movie.find(params[:id])
         render json: movie
     end 
@@ -25,8 +30,18 @@ class Api::V1::MoviesController < ApplicationController
 
     private
 
+    def set_genre
+        @genre = Genre.find(params[:genre_id])
+    end 
+
     def movie_params
         params.require(:movie).permit(:title, :description, :rating, :genre_id)
     end 
 end
 
+        # if params[:genre_id]
+        #     movie = Genre.find_by(id: params[:genre_id]).movies.distinct
+        #     genre = Genre.find_by(id: params[:genre_id])
+        # else 
+        #     movies = Movies.all
+        # end
--- flopify-api/config/routes.rb
+++ flopify-api/config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :movies
-      resources :genres
+      resources :genres do 
+        resources :movies
+      end 
     end 
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
