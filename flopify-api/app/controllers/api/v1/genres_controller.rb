06b654a03f23bbbbdd5467c484237d6b86989b1d
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-11-24 16:49:07 -0500

    sets up the genres controller

--- flopify-api/app/controllers/api/v1/genres_controller.rb
+++ flopify-api/app/controllers/api/v1/genres_controller.rb
@@ -1,19 +1,31 @@
 class Api::V1::GenresController < ApplicationController
-
     def index
+        genres = Genre.all
+        render json: genres 
     end 
 
     def create
+        genre = Genre.new(genre_params)
+        if genre.save
+            render json: genre
+        else 
+            render json: {error: 'Please try again'}
+        end 
     end
 
     def show
+        genre = Genre.find(params[:id])
+        render json: genre
     end 
 
     def destroy
+        genre = Genre.find(params[:id])
+        genre.destroy
     end 
 
     private
 
     def genre_params
+        params.require(:movie).permit(:name )
     end 
 end
