5653832d219de613b4930a57d34ea0fd26156239
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-11-25 13:53:19 -0500

    sets up store in index.js in flopify-frontend

--- flopify-frontend/src/App.js
+++ flopify-frontend/src/App.js
@@ -1,14 +1,13 @@
 import "./App.css";
 import React, { Component } from "react";
 
-// componentDidMount();
-// {
-//   fetch("http://localhost:3000/api/v1/genres")
-//     .then((response) => response.json())
-//     .then((data) => console.log(data));
-// }
-
 export default class App extends Component {
+  componentDidMount() {
+    fetch("http://localhost:3000/api/v1/genres")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
   render() {
     return <div className="App"></div>;
   }
--- flopify-frontend/src/index.js
+++ flopify-frontend/src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 
+import App from "./App";
+
+let store = createStore(reducer, applyMiddleware(thunk));
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
