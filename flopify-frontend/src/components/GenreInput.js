8f5871f3ec04b5aa6294edeee51aec368f75fe98
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-11-25 21:51:16 -0500

    sets up skeleton GenreInput.js and Genres.js components

---
+++ flopify-frontend/src/components/GenreInput.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class GenreInput extends Component {
+  render() {
+    return <div>GenreInput</div>;
+  }
+}
+
+export default GenreInput;
--- flopify-frontend/src/containers/GenresContainer.js
+++ flopify-frontend/src/containers/GenresContainer.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import Genres from "../components/Genres";
+import GenreInput from "../components/GenreInput";
 
 export default class GenresContainer extends Component {
   render() {
-    return <div>Here's a container</div>;
+    return (
+      <div>
+        <GenreInput />
+        <Genres />
+      </div>
+    );
   }
 }
