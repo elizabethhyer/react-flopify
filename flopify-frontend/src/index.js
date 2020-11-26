02eefe473287857beb611dacf293e4f407fd798d
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-11-25 19:23:48 -0500

    sets up genreReducer and imports it in index.js

--- flopify-frontend/src/index.js
+++ flopify-frontend/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import genreReducer from "./reducers/genreReducer";
 
 import App from "./App";
 
@@ -11,7 +12,7 @@ import App from "./App";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(reducer, applyMiddleware(thunk));
+let store = createStore(genreReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ flopify-frontend/src/reducers/genreReducer.js
@@ -0,0 +1,3 @@
+export default function genreReducer(state = { genres: [] }, action) {
+  return state;
+}
