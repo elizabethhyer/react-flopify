1dc7c2c4bc5af25720e2c00d24487a10f35a1132
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-11-27 17:42:18 -0500

    adds a controlled form into GenreInput.js

--- flopify-frontend/src/components/GenreInput.js
+++ flopify-frontend/src/components/GenreInput.js
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
 
 class GenreInput extends Component {
+  state = { name: "" };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
   render() {
-    return <div>GenreInput</div>;
+    return (
+      <div>
+        <form>
+          <label>Add a Genre:</label>
+          <br />
+          <input
+            type="text"
+            value={this.state.name}
+            name="name"
+            placeholder="New Genre"
+            onChange={this.handleChange}
+          />{" "}
+          <br /> <br />
+        </form>
+      </div>
+    );
   }
 }
 
