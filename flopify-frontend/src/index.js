0cc556aa6937052997f06559042a08d98a50aba7
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-11-27 22:08:45 -0500

    imports BrowserRouter and wraps App>

--- flopify-frontend/src/index.js
+++ flopify-frontend/src/index.js
@@ -4,11 +4,10 @@ import "./index.css";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 import genreReducer from "./reducers/genreReducer";
 
 import App from "./App";
-
-// let reducer = "";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -16,7 +15,9 @@ let store = createStore(genreReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
 
   document.getElementById("root")
