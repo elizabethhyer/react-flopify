0b94441765b3d52b06e2604a44f5770a1e27fbc2
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-11-28 17:29:27 -0500

    adds input fields for title, description and rating into MovieInput.js

--- flopify-frontend/src/components/MovieInput.js
+++ flopify-frontend/src/components/MovieInput.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class MovieInput extends Component {
   render() {
-    return <div>MOVIE INPUTZzzzzzz</div>;
+    return (
+      <div>
+        <form>
+          <label>Title:</label>
+          <input type="text" />
+          <br />
+          <br />
+          <label>Description:</label>
+          <input type="text" />
+          <br />
+          <br />
+          <label>(1-10) Rating:</label>
+          <input type="text" />
+          <br />
+          <br />
+          <input type="submit" />
+          <br />
+          <br />
+        </form>
+      </div>
+    );
   }
 }
 
-export default MovieInput;
+export default connect(null)(MovieInput);
