405ae13b2be88f05050fa87f1c63960c1a9582c4
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-11-28 15:10:07 -0500

    adds links to Genres.js

--- flopify-frontend/src/components/Genres.js
+++ flopify-frontend/src/components/Genres.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
 import Genre from "./Genre";
 
 const Genres = (props) => {
   return (
     <div>
       {props.genres.map((g) => (
-        <div key={g.id}>
-          <Genre genre={g} />
-        </div>
+        <li key={g.id}>
+          <Link to={`/genres/${g.id}`}>{g.name}</Link>
+        </li>
       ))}
     </div>
   );
