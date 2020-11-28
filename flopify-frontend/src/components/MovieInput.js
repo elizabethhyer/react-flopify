b714f6ee8da3a97fe5a69f2e64d3ab092d7045fb
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-11-28 17:35:36 -0500

    changes rating input to dropdown menu

--- flopify-frontend/src/components/MovieInput.js
+++ flopify-frontend/src/components/MovieInput.js
@@ -2,20 +2,43 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class MovieInput extends Component {
+  state = {
+    title: "",
+    description: "",
+    rating: "",
+  };
+
+  handleChange = () => {};
+
   render() {
     return (
       <div>
         <form>
           <label>Title:</label>
-          <input type="text" />
+          <input type="text" name="title" value={this.state.title} />
           <br />
           <br />
           <label>Description:</label>
-          <input type="text" />
+          <input
+            type="text"
+            name="description"
+            value={this.state.description}
+          />
           <br />
           <br />
           <label>(1-10) Rating:</label>
-          <input type="text" />
+          <select name="rating" value={this.state.value}>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+            <option>10</option>
+          </select>
           <br />
           <br />
           <input type="submit" />
