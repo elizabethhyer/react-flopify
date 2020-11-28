726d269a94c395e6edf8ddabc4e2bfb55a454f06
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-11-27 21:43:04 -0500

    adds an add_genre case to genre_reducer.js

--- flopify-frontend/src/reducers/genreReducer.js
+++ flopify-frontend/src/reducers/genreReducer.js
@@ -3,6 +3,9 @@ export default function genreReducer(state = { genres: [] }, action) {
     case "FETCH_GENRES":
       return { genres: action.payload };
 
+    case "ADD_GENRE":
+      return { ...state, genres: [...state.genres, action.payload] };
+
     default:
       return state;
   }
