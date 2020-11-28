b5444c1aacccfecc273b4db2720b4b2595d9dd71
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-11-28 17:13:00 -0500

    adds title and rating to Movies.js

--- flopify-frontend/src/components/Movies.js
+++ flopify-frontend/src/components/Movies.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
 
 const Movies = (props) => {
-  return <div>Mewvies</div>;
+  return (
+    <div>
+      {props.movies &&
+        props.movies.map((m) => (
+          <li key={m.id}>
+            Title: {m.title} <br />
+            Rating: {m.rating} / 10 <br />
+          </li>
+        ))}
+    </div>
+  );
 };
 
 export default Movies;
